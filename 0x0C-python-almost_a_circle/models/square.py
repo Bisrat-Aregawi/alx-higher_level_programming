1527f49a8f4f81e0bf620a773ff40dd5a62f50cd
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-11-09 18:36:20 +0000

    Task 12 answer for 0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,30 +37,6 @@ class Square(rectangle.Rectangle):
         """
         return super().width
 
-    @property
-    def x(self):
-        """Return x coordinate of square.
-
-        Args:
-            self (object): Refers to object instantiated
-
-        Returns:
-            The private attribute 'x'
-        """
-        return super().x
-
-    @property
-    def y(self):
-        """Return y coordinate of square.
-
-        Args:
-            self (object): Refers to object instantiated
-
-        Returns:
-            The private attribute 'y'
-        """
-        return super().y
-
     @size.setter
     def size(self, value):
         """Sets the @width and @height attribs to @value
@@ -74,6 +50,7 @@ class Square(rectangle.Rectangle):
         """
         self.width = value
         self.height = value
+        return None
 
     def __str__(self):
         """Display object representing string.
@@ -86,5 +63,42 @@ class Square(rectangle.Rectangle):
         """
         string = "[Square] ({}) {}/{}".format(self.id, self.x, self.y)
         string += " - {}".format(self.size)
-
         return string
+
+    def update(self, *args, **kwargs):
+        """Update attributes of Square object.
+
+        Args:
+            First element of @args is 'id'
+            Second element of @args is 'width'
+            Third element of @args is 'height'
+            Fourth element of @args is 'x'
+            Fifth element of @args is 'y'
+
+        Returns:
+            None
+        """
+        arg_position = 0
+        if len(args) != 0:
+            for elem in args:
+                if arg_position == 0:
+                    self.id = elem
+                elif arg_position == 1:
+                    self.size = elem
+                elif arg_position == 2:
+                    self.x = elem
+                elif arg_position == 3:
+                    self.y = elem
+                arg_position = arg_position + 1
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+        return None
+    pass
