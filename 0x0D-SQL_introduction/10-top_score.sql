88f31228f7f6f72575d6ed07ff02e21669f02a0e
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2021-12-09 16:50:20 +0300

    Add SQL script to list ordered records
    For task 10 of 0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List all records (rows) of the table `second_table`
+-- The order of printing is score field, name field
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
